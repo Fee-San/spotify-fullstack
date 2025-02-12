8bfb85ce801988f0e3d490375fff42f45aab0d2f
Felipe Santos <contato.felipesantos27@gmail.com>
2025-02-12 20:00:50 -0300

    Home page

---
+++ src/App2.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de componentes
+// PascalCase
+
+// Nomeação de Variável, Função...
+// camelCase
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado 
+
+// Self closing tag
+// <Header></Header>
+// <Header/>
+
+// Nomeação de classes em css
+// Metodologia BEM:
+// Blocks
+// Elements 
+// Modifiers
+// bloco__elemento--modificador
+// headre__link--small
+
+// Nomes compostos são separados por - 
+// item-list__header
+
+// Tag vazia em React se chama Fragment ou Fragmento
